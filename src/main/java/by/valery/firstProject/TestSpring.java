7fba0f820eebc457a8387b3448b1b09e544e7acf
BANK <valeriy_v_r@mail.ru>
2023-04-03 14:33:43 +0300

    Change TestSpring_1 for Lesson 12

--- src/main/java/by/valery/firstProject/TestSpring.java
+++ src/main/java/by/valery/firstProject/TestSpring.java
@@ -12,7 +12,7 @@ public class TestSpring {
 
         musicPlayer.playMusic(GenreOfMusic.ROCK);
 
-        System.out.println("Hi");
+        System.out.println("Hi, bro");
 
         context.close();
     }
