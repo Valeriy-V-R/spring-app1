c55a228b4cd26668b2a5dc690f0ecfaa88fd9d88
Valeriy-V-R <valeriy_v_r@mail.ru>
2023-04-04 10:28:32 +0300

    Spring Lesson 13 Конф-я с помощью Java, создан конф ява файл SpringConfig.java

--- src/main/java/by/valery/firstProject/SpringConfig.java
+++ src/main/java/by/valery/firstProject/SpringConfig.java
@@ -1,4 +1,12 @@
 package by.valery.firstProject;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("by.valery.firstProject")
+@PropertySource("musicPlayer.properties")
 public class SpringConfig {
 }
